3d90c3f485ae0d255441ebcd45cc67a756a6eead
Zasha <zahid.shaikh@live.com>
2023-05-23 07:58:06 +0300

    uploading cursor take 1

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,6 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
+      <title>AgorA - Solana's first cNFT Marketplace</title>
       <Head />
       <body>
         <Main />
