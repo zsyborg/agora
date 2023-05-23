a71ad5d67bfee5f7f9a1033e66e9d0a3ff44f734
Zasha <zahid.shaikh@live.com>
2023-05-23 08:20:20 +0300

    Pampering with Branding

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,6 +4,16 @@ export default function Document() {
   return (
     <Html lang="en">
       <title>AgorA - Solana's first cNFT Marketplace</title>
+      <meta
+            name="keywords"
+            content="AgorA Marketplace Solana cNFT"
+          />
+          <meta
+            name="description"
+            content="AgorA - Solana's First cNFT Marketplace to bring forth a change and put back the Soul in SOL"
+          />
+          <meta name="author" content="AgoriA" />
+          <link rel="shortcut icon" href="/img/favicon.ico" />
       <Head />
       <body>
         <Main />
